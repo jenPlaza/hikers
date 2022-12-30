38dd73fb3dac9998d14153eb44f139c33e65e54f
Jennifer Plaza <jpplaza@student.fullsail.edu>
2022-12-29 20:11:06 -0500

    added blog

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="body">
-    <!-- <Articles /> -->
+    <Articles />
   </section>
 </template>
 
 <script>
-// import Articles from '../components/Blog/articles';
+import Articles from '../components/Blog/articles';
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'blog',
   components: {
-    // Articles,
+    Articles,
   },
 };
 </script>
